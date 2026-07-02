7a86cebefc80ce1a757955e0569be4b7624ba098
Pranav-Dhadwal <dhadwal.pranav01@gmail.com>
2026-07-03 04:47:55 +0530

    Added app.py

---
+++ DecisionLens/app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+
+
+
+st.title("📊 DecisionLens")
+
+st.markdown("""
+Welcome to **DecisionLens**.
+
+This application helps analyze whether a business decision
+has a statistically significant impact using hypothesis testing.
+
+### Workflow
+1. Generate Dataset
+2. Exploratory Data Analysis (EDA)
+3. Hypothesis Testing
+4. Business Recommendation
+
+👈 Select a page from the sidebar to get started.
+""")
