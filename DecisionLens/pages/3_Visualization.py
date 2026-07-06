2d59de8489d4b424515f3c53f12db254642abd7d
Pranav-Dhadwal <dhadwal.pranav01@gmail.com>
2026-07-06 14:28:31 +0530

    Enhanced Visualization part with some Business Charts

--- DecisionLens/pages/3_Visualization.py
+++ DecisionLens/pages/3_Visualization.py
@@ -3,9 +3,7 @@ import pandas as pd
 import streamlit as st
 from src.visualization import *
 
-st.title("📊 Interactive Visualization")
-
-st.markdown("---")
+# Data file import 
 path = os.path.join("./","data","raw","simulated_data.csv")
 
 if "dataset" in st.session_state:
@@ -16,46 +14,89 @@ else:
 numeric_cols = data.select_dtypes(include="number").drop(columns='customer_id').columns.tolist()
 categorical_cols = data.select_dtypes(include="object").columns.tolist()
 
-left, right = st.columns([1,2])
+st.header("📊 Business Insights")
+st.divider()
 
-with left:
-    chart = st.selectbox(
-        "Visualization Type",
-        ["Bar Chart","Pie Chart","Histogram","Box Plot","Scatter Plot","Line Plot"]
-    )
-    if chart=="Bar Chart":
-        c = st.selectbox("Categorical Feature", categorical_cols)
-    elif chart=="Pie Chart":
-        c = st.selectbox("Categorical Feature", categorical_cols)
-    elif chart=="Histogram":
-        c = st.selectbox("Numerical Feature", numeric_cols)
-        bins = st.slider("Bins",5,50,20)
-    elif chart=="Box Plot":
-        x = st.selectbox("Categorical Feature", categorical_cols)
-        y = st.selectbox("Numerical Feature", numeric_cols)
-    elif chart=="Scatter Plot":
-        x = st.selectbox("X-axis", numeric_cols)
-        y = st.selectbox("Y-axis", numeric_cols, index=min(1,len(numeric_cols)-1))
-        hue = st.selectbox("Color (Optional)", ["None"]+categorical_cols)
-    elif chart=="Line Plot":
-        x = st.selectbox("X-axis", numeric_cols)
-        y = st.selectbox("Y-axis", numeric_cols, index=min(1,len(numeric_cols)-1))
+st.subheader("Customer Demographics")
+with st.expander("View"):
+    plot_customer_demographics(data)
 
-with right:
-    if chart=="Bar Chart":
-        plot_bar(data,c)
-    elif chart=="Pie Chart":
-        plot_pie(data,c)
-    elif chart=="Histogram":
-        plot_histogram(data,c,bins)
-    elif chart=="Box Plot":
-        plot_boxplot(data,x,y)
-    elif chart=="Scatter Plot":
-        plot_scatter(data,x,y,None if hue=="None" else hue)
-    elif chart=="Line Plot":
-        plot_line(data,x,y)
+st.divider()
 
-st.info("""
-Use the controls on the left to explore the dataset.
-Different chart types automatically expose compatible features.
-""")
+st.subheader("Customer Demographics")
+with st.expander("View"):
+    plot_customer_behaviour(data)
+
+st.divider()
+
+st.subheader("Customer Engagement")
+with st.expander("View"):
+    plot_customer_engagement(data)
+
+st.divider()
+
+st.subheader("Correlation Heatmap")
+with st.expander("View"):
+    plot_correlation(data)
+
+st.divider()
+
+st.subheader("Experiment Analysis")
+with st.expander("View"):
+
+    plot_experiment_overview(data)
+
+st.divider()
+
+st.header("🎮 Try It Yourself")
+
+# Your interactive visualization component
+
+with st.expander("Try it Yourself"):
+    st.subheader("Create your own visulaization by selecting a chart type and compatible feature")
+
+    st.markdown("---")
+
+    left, right = st.columns([1,2])
+
+    with left:
+        chart = st.selectbox(
+            "Visualization Type",
+            ["Bar Chart","Pie Chart","Histogram","Box Plot","Scatter Plot","Line Plot"]
+        )
+        if chart=="Bar Chart":
+            c = st.selectbox("Categorical Feature", categorical_cols)
+        elif chart=="Pie Chart":
+            c = st.selectbox("Categorical Feature", categorical_cols)
+        elif chart=="Histogram":
+            c = st.selectbox("Numerical Feature", numeric_cols)
+            bins = st.slider("Bins",5,50,20)
+        elif chart=="Box Plot":
+            x = st.selectbox("Categorical Feature", categorical_cols)
+            y = st.selectbox("Numerical Feature", numeric_cols)
+        elif chart=="Scatter Plot":
+            x = st.selectbox("X-axis", numeric_cols)
+            y = st.selectbox("Y-axis", numeric_cols, index=min(1,len(numeric_cols)-1))
+            hue = st.selectbox("Color (Optional)", ["None"]+categorical_cols)
+        elif chart=="Line Plot":
+            x = st.selectbox("X-axis", numeric_cols)
+            y = st.selectbox("Y-axis", numeric_cols, index=min(1,len(numeric_cols)-1))
+
+    with right:
+        if chart=="Bar Chart":
+            plot_bar(data,c)
+        elif chart=="Pie Chart":
+            plot_pie(data,c)
+        elif chart=="Histogram":
+            plot_histogram(data,c,bins)
+        elif chart=="Box Plot":
+            plot_boxplot(data,x,y)
+        elif chart=="Scatter Plot":
+            plot_scatter(data,x,y,None if hue=="None" else hue)
+        elif chart=="Line Plot":
+            plot_line(data,x,y)
+
+    st.info("""
+    Use the controls on the left to explore the dataset.
+    Different chart types automatically expose compatible features.
+    """)
--- DecisionLens/src/visualization.py
+++ DecisionLens/src/visualization.py
@@ -2,6 +2,148 @@ import streamlit as st
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+# Customer demographics 
+def plot_customer_demographics(data):
+
+    fig, axes = plt.subplots(1, 2, figsize=(14, 5))
+
+    sns.countplot(data=data, x="gender", ax=axes[0])
+
+    axes[0].set_title("Customer Distribution by Gender")
+    axes[0].set_xlabel("Gender")
+    axes[0].set_ylabel("Customers")
+    axes[0].bar_label(axes[0].containers[0])
+
+    sns.countplot(data=data, x="country", ax=axes[1])
+
+    axes[1].set_title("Customer Distribution by Country")
+    axes[1].set_xlabel("Country")
+    axes[1].set_ylabel("Customers")
+
+    axes[1].tick_params(axis="x", rotation=25)
+    axes[1].bar_label(axes[1].containers[0])
+
+    plt.tight_layout()
+    st.pyplot(fig)
+
+# Customer Behaviour 
+def plot_customer_behaviour(data):
+
+    fig, axes = plt.subplots(1, 2, figsize=(14,5))
+
+    sns.countplot(
+        data=data,
+        x="device",
+        ax=axes[0]
+    )
+
+    axes[0].set_title("Browsing Device")
+    axes[0].bar_label(axes[0].containers[0])
+
+    sns.countplot(
+        data=data,
+        x="traffic_source",
+        ax=axes[1]
+    )
+
+    axes[1].set_title("Traffic Source")
+    axes[1].tick_params(axis="x", rotation=25)
+    axes[1].bar_label(axes[1].containers[0])
+
+    plt.tight_layout()
+    st.pyplot(fig)
+
+# Customer Engagement 
+def plot_customer_engagement(data):
+
+    fig, axes = plt.subplots(1,3, figsize=(18,5))
+
+    sns.histplot(
+        data=data,
+        x="session_duration",
+        kde=True,
+        ax=axes[0]
+    )
+
+    axes[0].set_title("Session Duration")
+
+    sns.histplot(
+        data=data,
+        x="pages_viewed",
+        kde=True,
+        ax=axes[1]
+    )
+
+    axes[1].set_title("Pages Viewed")
+
+    sns.histplot(
+        data=data,
+        x="previous_orders",
+        kde=True,
+        ax=axes[2]
+    )
+
+    axes[2].set_title("Previous Orders")
+
+    plt.tight_layout()
+
+    st.pyplot(fig)
+
+
+# Experiment overview 
+def plot_experiment_overview(data):
+
+    fig, axes = plt.subplots(1,2, figsize=(14,5))
+
+    sns.boxplot(
+        data=data,
+        x="experiment_group",
+        y="purchase_prob",
+        ax=axes[0]
+    )
+
+    axes[0].set_title("Purchase Probability by Group")
+
+    sns.barplot(
+        data=data,
+        x="experiment_group",
+        y="purchase_prob",
+        estimator="mean",
+        errorbar=None,
+        ax=axes[1]
+    )
+
+    axes[1].set_title("Average Purchase Probability")
+
+    for container in axes[1].containers:
+        axes[1].bar_label(container, fmt="%.3f")
+
+    plt.tight_layout()
+
+    st.pyplot(fig)
+
+# Correlation 
+def plot_correlation(data):
+    fig, ax = plt.subplots(figsize=(8, 6))
+
+    corr = data.select_dtypes(include="number").corr()
+
+    sns.heatmap(
+        corr,
+        annot=True,
+        cmap="Blues",
+        fmt=".2f",
+        linewidths=.5,
+        ax=ax
+    )
+
+    ax.set_title("Correlation Between Numerical Features")
+
+    plt.tight_layout()
+    st.pyplot(fig)
+
+    
+# interactive visualizatoin part 
 def plot_bar(data, col):
     fig, ax = plt.subplots(figsize=(8,5))
 
